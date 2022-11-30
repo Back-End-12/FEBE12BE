573974162f01f9ba329f3ca6f1a9850b3ba80437
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 22:40:24 +0700

    Update kegiatanRoutes.js

--- routes/kegiatanRoutes.js
+++ routes/kegiatanRoutes.js
@@ -1,18 +1,12 @@
-const express = require("express");
-const router = express.Router();
+const { Router } = require('express');
+const kegiatanController = require('../controllers/kegiatanController');
+const {isAuthenticated} = require("../middleware/auth");
+const router = Router();
 
-const {
-    uploadKegiatan,
-    getAllKegiatan,
-    getKegiatanByID,
-    deleteKegiatanByID,
-    updateKegiatanByID
-} = require("../controllers/kegiatanController");
-
-router.post("/postKegiatan", uploadKegiatan)
-router.get("/AllKegiatan", getAllKegiatan);
-router.get("/:id", getKegiatanByID);
-router.delete("/:id", deleteKegiatanByID);
-router.patch("/:id", updateKegiatanByID);
+router.get('/kegiatan', kegiatanController.getAllKegiatan);
+router.get('/kegiatan/:id', kegiatanController.getKegiatanByID);
+router.post('/postKegiatan', isAuthenticated, kegiatanController.uploadKegiatan);
+router.delete('/kegiatan/delete/:id', isAuthenticated, kegiatanController.deleteKegiatanByID);
+router.patch("/kegiatan/edit/:id", isAuthenticated, kegiatanController.updateKegiatanByID);
 
 module.exports = router;
