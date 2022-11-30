40e6d6cc98a3091a93a22c9a7813f06667be2c6f
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 22:44:44 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const kegiatanRoutes = require('./routes/kegiatanRoutes');
 const cors = require('cors');
 
 const app = express();
@@ -12,18 +13,17 @@ mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection 
 
 // middleware
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 //import
-const kegiatanRoutes = require('./routes/kegiatanRoutes')
 
 // routes
-app.use(authRoutes);
-app.use(userRoutes);
-app.use(kegiatanRoutes);
 
+app.use(authRoutes);
+app.use(kegiatanRoutes);
+app.use(userRoutes);
 
 app.listen(process.env.PORT||5173, function () {
     console.log(`listen on port ${process.env.PORT || 5173}`);
