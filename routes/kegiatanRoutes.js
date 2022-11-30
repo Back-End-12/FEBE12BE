afa1e17b61970eeb1a9dd6d10e2b39b83eb9518f
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-11-30 22:55:15 +0700

    Update kegiatanRoutes.js

--- routes/kegiatanRoutes.js
+++ routes/kegiatanRoutes.js
@@ -7,6 +7,6 @@ router.get('/kegiatan', kegiatanController.getAllKegiatan);
 router.get('/kegiatan/:id', kegiatanController.getKegiatanByID);
 router.post('/postKegiatan', [isAuthenticated, isAdmin], kegiatanController.uploadKegiatan);
 router.delete('/kegiatan/delete/:id', [isAuthenticated, isAdmin], kegiatanController.deleteKegiatanByID);
-router.patch("/kegiatan/edit/:id", [isAuthenticated, isAdmin], kegiatanController.updateKegiatanByID);
+router.patch('/kegiatan/edit/:id', [isAuthenticated, isAdmin], kegiatanController.updateKegiatanByID);
 
 module.exports = router;
