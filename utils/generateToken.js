3108d5606d02ab430eec468df85d00ece40b6dca
Dustin Bayu Herlambang <114048736+Zerexxn@users.noreply.github.com>
2022-12-01 22:10:59 +0700

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,9 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, 'febe12', {
-    expiresIn: "3d"
-  });
-};
+
+const generateToken = async (user, statusCode, res) =>{
+
+    const token = await user.jwtGenerateToken();
+
+    const options = {
+        httpOnly: true,
+        expires: new Date(Date.now() + process.env.EXPIRE_TOKEN)
+    };
+
+    res
+    .status(statusCode)
+    .cookie('token', token, options )
+    .json({success: true, token})
+}
+
+//const generateToken = (id) => {
+ // return jwt.sign({ id }, 'febe12', {
+   // expiresIn: "3d"
+ // });
+// };
 
 module.exports = generateToken;
