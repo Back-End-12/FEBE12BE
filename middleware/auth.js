84cf0118b1a7c80f090eef0b0e2ec1f5348f9ee6
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-12-01 14:17:27 +0700

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,26 +3,54 @@ const User = require("../models/User");
 const ErrorResponse = require('../utils/errorResponse');
 
 
-// check if user is authenticated
-exports.isAuthenticated = async (req, res, next) =>{
+// // check if user is authenticated
+// exports.isAuthenticated = async (req, res, next) =>{
 
-    const {authorization} = req.headers;
+//     const {authorization} = req.headers;
 
-    // make sure token exists
-    if (!authorization){
-        return next (new ErrorResponse('You must log in to access this ressource', 401));
-    }
+//     // make sure token exists
+//     if (!authorization){
+//         return next (new ErrorResponse('You must log in to access this ressource', 401));
+//     }
 
-    try {
-        //verify token
-        const decoded = jwt.verify(authorization, process.env.JWT_SECRET);
-        req.user = await User.findById(decoded.id);
-        next();
+//     try {
+//         //verify token
+//         const decoded = jwt.verify(authorization, process.env.JWT_SECRET);
+//         req.user = await User.findById(decoded.id);
+//         next();
 
-    } catch (error) {
-        return next (new ErrorResponse('You must log in to access this ressource', 401));
-    }
-}
+//     } catch (error) {
+//         return next (new ErrorResponse('You must log in to access this ressource', 401));
+//     }
+// }
+
+exports.isAuthenticated = async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+      ) {
+        try {
+          token = req.headers.authorization.split(" ")[1];
+    
+          //decodes token id
+          const decoded = jwt.verify(token, 'febe15');
+    
+          req.user = await User.findById(decoded.id).select("-password");
+    
+          next();
+        } catch (error) {
+          res.status(401);
+          throw new Error("Not authorized, token failed");
+        }
+      }
+    
+      if (!token) {
+        res.status(401);
+        throw new Error("Not authorized, no token");
+      }
+})
 
 // admin middleware
 exports.isAdmin = async(req, res, next) => {
