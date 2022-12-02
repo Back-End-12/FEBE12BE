358ab4731e351e91d9d7a60d81b107d2ab87f813
shehaannisa <103401710+shehaannisa@users.noreply.github.com>
2022-12-02 17:28:51 +0700

    Update kegiatanRoutes.js

--- routes/kegiatanRoutes.js
+++ routes/kegiatanRoutes.js
@@ -4,12 +4,12 @@ const { createKegiatan, getAllKegiatan, deleteKegiatan, updateKegiatan } = requi
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
 
 
-router.post('/Kegiatan/create', isAuthenticated, isAdmin, createKegiatan);
-//router.post('/Kegiatan/create', createKegiatan);
+//router.post('/Kegiatan/create', isAuthenticated, isAdmin, createKegiatan);
+router.post('/Kegiatan/create', createKegiatan);
 router.get("/AllKegiatan", getAllKegiatan);
-router.delete('/Kegiatan/delete/:id', isAuthenticated, isAdmin, deleteKegiatan);
-//router.delete('/Kegiatan/delete/:id', deleteKegiatan);
-router.put('/Kegiatan/update/:id', isAuthenticated, isAdmin, updateKegiatan);
-//router.put('/Kegiatan/update/:id', updateKegiatan);
+//router.delete('/Kegiatan/delete/:id', isAuthenticated, isAdmin, deleteKegiatan);
+router.delete('/Kegiatan/delete/:id', deleteKegiatan);
+//router.put('/Kegiatan/update/:id', isAuthenticated, isAdmin, updateKegiatan);
+router.put('/Kegiatan/update/:id', updateKegiatan);
 
 module.exports = router;
